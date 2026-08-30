384d918cd6a80b1dd36baab0657519339e5c01eb
volkovaevln <volkova.evln@gmail.com>
2026-08-30 18:05:09 +0300

    Implement file handler

---
+++ src/app/FileHandler.java
@@ -0,0 +1,34 @@
+package app;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler {
+
+    private final static String BASE_PATH = "files/";
+
+    public String writeFile(String fileName, String fileContent) {
+        try (FileWriter fw = new FileWriter(BASE_PATH + fileName + ".txt")) {
+            fw.write(fileContent);
+            return "Success.";
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    public String readFile(String path) {
+        try (FileReader reader = new FileReader(path)) {
+            StringBuilder stringBuilder = new StringBuilder();
+            int sym;
+
+            while ((sym = reader.read()) != -1) {
+                stringBuilder.append((char) sym);
+            }
+            return stringBuilder.toString();
+
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+}
